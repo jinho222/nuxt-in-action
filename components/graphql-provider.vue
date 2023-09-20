32fe107bc5827764e3592151efb7f497d3fe99e6
jinho222 <jinho020@yonsei.ac.kr>
2023-09-20 15:33:00 +0900

    add urql devtools

--- components/graphql-provider.vue
+++ components/graphql-provider.vue
@@ -1,14 +1,10 @@
 <script setup lang="ts">
-import {
-  cacheExchange,
-  createClient,
-  fetchExchange,
-  provideClient,
-} from "@urql/vue";
+import { cacheExchange, createClient, fetchExchange, provideClient } from "@urql/vue";
+import { devtoolsExchange } from "@urql/devtools";
 
 const client = createClient({
   url: "https://countries.trevorblades.com/",
-  exchanges: [cacheExchange, fetchExchange],
+  exchanges: [devtoolsExchange, cacheExchange, fetchExchange],
 });
 
 provideClient(client);
