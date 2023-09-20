8dc6b3f344286e960b9d480153a1246ddba9ea87
jinho222 <jinho020@yonsei.ac.kr>
2023-09-20 15:34:29 +0900

    use urql devtools only in dev

--- components/graphql-provider.vue
+++ components/graphql-provider.vue
@@ -4,7 +4,7 @@ import { devtoolsExchange } from "@urql/devtools";
 
 const client = createClient({
   url: "https://countries.trevorblades.com/",
-  exchanges: [devtoolsExchange, cacheExchange, fetchExchange],
+  exchanges: [...(import.meta.env.DEV ? [devtoolsExchange] : []), cacheExchange, fetchExchange],
 });
 
 provideClient(client);
